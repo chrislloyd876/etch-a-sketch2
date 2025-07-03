d4416802d89fc8933484685d6eec0c6b48bf02b1
chrislloyd876 <chrislloydlawrence@gmail.com>
2025-07-02 21:23:12 -0500

    Completed application

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Revision of etch-a-sketch app
 
 1. Created the repository and the basic template.
 - Added numbers to the squares to ensure the count.
+
+2. Completed the application. Didn't feel like I learned much from this project. Need better planning and execution.
--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
 const container = document.querySelector("#container");
 
+const resetButton = document.createElement('button');
+resetButton.classList.add('resetButton');
+resetButton.textContent = "RESET";
+container.appendChild(resetButton);
+
 for(let square_row = 0;square_row < 16; square_row++){
     const rowBox = document.createElement("div");
     rowBox.classList.add('rowBox');
@@ -9,5 +14,48 @@ for(let square_row = 0;square_row < 16; square_row++){
         colBox.classList.add('colBox');
         colBox.textContent = `${col_row + 1}`;
         rowBox.appendChild(colBox);
+        colBox.onmouseover = function(){
+            let rCol = randomColor();
+            colBox.style.backgroundColor = rCol;
+        }
     }
 }
+
+function getSquares(){
+    let square_num = Number(prompt("How many squares do you want on the sides? Max. 100"));
+    if(square_num > 100){
+        alert("OOPS! That's too many.Try again");
+    }
+    const rows = document.querySelectorAll(".rowBox");
+
+    rows.forEach(row => {
+        row.remove();
+    });
+
+    for(let square_row = 0;square_row < square_num; square_row++){
+        const rowBox = document.createElement("div");
+        rowBox.classList.add('rowBox');
+        container.appendChild(rowBox);
+        for(let col_row = 0; col_row < square_num; col_row++){
+            const colBox = document.createElement("div");
+            colBox.classList.add('colBox');
+            colBox.textContent = `${col_row + 1}`;
+            rowBox.appendChild(colBox);
+            colBox.onmouseover = function(){
+                let rCol = randomColor();
+                colBox.style.backgroundColor = rCol;
+            }
+        }
+    }
+}
+
+function randomColor(){
+    let color1 = Math.floor(Math.random() * 256);
+    let color2 = Math.floor(Math.random() * 256);
+    let color3 = Math.floor(Math.random() * 256);
+
+    let rColor = "rgb(" + color1 + "," + color2 + "," + color3 + ")";
+    return rColor;
+}
+
+resetButton.addEventListener("click", getSquares);
